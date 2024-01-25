52058c06962a400ed2a8de13e70fb2c6bf96dbc6
Vasilije <8619304+Vasilije1990@users.noreply.github.com>
2024-01-25 12:31:08 +0100

    Rename the build package name and refactored the local runner

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,7 +19,6 @@ if [ $? -ne 0 ]; then
     echo "Error: create_database.py failed"
     exit 1
 fi
-
 # Start Gunicorn
 echo "Starting Gunicorn"
 gunicorn -w 3 -k uvicorn.workers.UvicornWorker -t 30000 --bind=0.0.0.0:8000 --bind=0.0.0.0:443 --log-level debug api:app
