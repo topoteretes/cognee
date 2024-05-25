423a7c2e209f6ea289bd89fd068d3af8be979dd2
Vasilije <8619304+Vasilije1990@users.noreply.github.com>
2024-05-25 17:30:14 +0200

    try few things

--- cognee/root_dir.py
+++ cognee/root_dir.py
@@ -1,11 +1,22 @@
 from os import path
 import logging
-ROOT_DIR = path.dirname(path.abspath(__file__))
+from pathlib import Path
+logging.basicConfig(level=logging.DEBUG)
+# ROOT_DIR = path.dirname(path.abspath(__file__))
+#
+# logging.debug("ROOT_DIR: ", ROOT_DIR)
+#
+# def get_absolute_path(path_from_root: str) -> str:
+#     logging.debug("abspath: ", path.abspath(path.join(ROOT_DIR, path_from_root)))
+#
+#
+#     return path.abspath(path.join(ROOT_DIR, path_from_root))
+ROOT_DIR = Path(__file__).resolve().parent
 
-logging.debug("ROOT_DIR: ", ROOT_DIR)
+logging.basicConfig(level=logging.DEBUG)
+logging.debug("ROOT_DIR: %s", ROOT_DIR)
 
 def get_absolute_path(path_from_root: str) -> str:
-    logging.debug("abspath: ", path.abspath(path.join(ROOT_DIR, path_from_root)))
-
-
-    return path.abspath(path.join(ROOT_DIR, path_from_root))
+    absolute_path = ROOT_DIR / path_from_root
+    logging.debug("abspath: %s", absolute_path.resolve())
+    return str(absolute_path.resolve())
