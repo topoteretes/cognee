bbfec1c044c96ba082cd5cc06e2f41dea1411a71
Vasilije <8619304+Vasilije1990@users.noreply.github.com>
2024-05-26 14:05:49 +0200

    Updates to the configs

--- cognee/api/v1/add/add.py
+++ cognee/api/v1/add/add.py
@@ -73,7 +73,7 @@ async def add_files(file_paths: List[str], dataset_name: str):
         else:
             processed_file_paths.append(file_path)
 
-    db = duckdb.connect(relational_config.database_directory_path)
+    db = duckdb.connect(relational_config.db_file_path)
 
     destination = dlt.destinations.duckdb(
         credentials = db,
