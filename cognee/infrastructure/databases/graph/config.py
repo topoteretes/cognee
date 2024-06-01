b5c6628cfe5a28b86a9349c954b46bbeba77e9ce
vujke <stefanvujovic93@gmail.com>
2024-06-01 18:43:04 +0200

    add_coginitive_layer_graphs fix

--- cognee/infrastructure/databases/graph/config.py
+++ cognee/infrastructure/databases/graph/config.py
@@ -1,9 +1,11 @@
 """ This module contains the configuration for the graph database. """
+
 import os
 from functools import lru_cache
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from cognee.infrastructure.databases.relational.config import get_relationaldb_config
-from cognee.shared.data_models import DefaultGraphModel, GraphDBType
+from cognee.shared.data_models import DefaultGraphModel, GraphDBType, KnowledgeGraph
+
 
 class GraphConfig(BaseSettings):
     graph_filename: str = "cognee_graph.pkl"
@@ -12,11 +14,13 @@ class GraphConfig(BaseSettings):
     graph_database_username: str = ""
     graph_database_password: str = ""
     graph_database_port: int = 123
-    graph_file_path: str = os.path.join(get_relationaldb_config().db_path, graph_filename)
-    graph_engine: object =  GraphDBType.NETWORKX
-    graph_model: object = DefaultGraphModel
+    graph_file_path: str = os.path.join(
+        get_relationaldb_config().db_path, graph_filename
+    )
+    graph_engine: object = GraphDBType.NETWORKX
+    graph_model: object = KnowledgeGraph
 
-    model_config = SettingsConfigDict(env_file = ".env", extra = "allow")
+    model_config = SettingsConfigDict(env_file=".env", extra="allow")
 
     def to_dict(self) -> dict:
         return {
@@ -28,9 +32,9 @@ class GraphConfig(BaseSettings):
             "graph_database_username": self.graph_database_username,
             "graph_database_password": self.graph_database_password,
             "graph_database_port": self.graph_database_port,
-            "graph_engine": self.graph_engine
-
+            "graph_engine": self.graph_engine,
         }
+
 
 @lru_cache
 def get_graph_config():
