29ba4809fed0ee8095b8f56749470c93e2605712
Boris Arzentar <borisarzentar@gmail.com>
2024-06-03 14:08:46 +0200

    fix: add default values for graphistry

--- cognee/base_config.py
+++ cognee/base_config.py
@@ -6,8 +6,8 @@ from cognee.shared.data_models import MonitoringTool
 class BaseConfig(BaseSettings):
     data_root_directory: str = get_absolute_path(".data")
     monitoring_tool: object = MonitoringTool.LANGFUSE
-    graphistry_username: str
-    graphistry_password: str
+    graphistry_username: str = None
+    graphistry_password: str = None
 
     model_config = SettingsConfigDict(env_file = ".env", extra = "allow")
 
