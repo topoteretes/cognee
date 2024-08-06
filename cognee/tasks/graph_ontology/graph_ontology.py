82ac9fc26a01acba224d65d59559522155061e9b
Vasilije <8619304+Vasilije1990@users.noreply.github.com>
2024-08-06 18:28:32 +0200

    Fix to the task

---
+++ cognee/tasks/graph_ontology/graph_ontology.py
@@ -0,0 +1,22 @@
+from typing import Type
+from pydantic import BaseModel
+
+from cognee.modules.data.processing.chunk_types.DocumentChunk import DocumentChunk
+from cognee.shared.data_models import KnowledgeGraph
+from cognee.infrastructure.databases.graph import get_graph_engine
+from cognee.modules.data.extraction.knowledge_graph.add_model_class_to_graph import add_model_class_to_graph
+
+
+async def establish_graph_topology(data_chunks: list[DocumentChunk], topology_model: Type[BaseModel]):
+    if topology_model == KnowledgeGraph:
+        return data_chunks
+
+    graph_engine = await get_graph_engine()
+
+    await add_model_class_to_graph(topology_model, graph_engine)
+
+    return data_chunks
+
+
+def generate_node_id(node_id: str) -> str:
+    return node_id.upper().replace(" ", "_").replace("'", "")
