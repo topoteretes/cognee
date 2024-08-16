81085550d07a13798bacd2b53c63ef2fb29e3050
Boris Arzentar <borisarzentar@gmail.com>
2024-08-16 20:43:59 +0200

    fix: print db info for debug

--- cognee/infrastructure/databases/relational/create_relational_engine.py
+++ cognee/infrastructure/databases/relational/create_relational_engine.py
@@ -10,6 +10,12 @@ def create_relational_engine(
     db_user: str,
     db_password: str,
 ):
+    print(f"DB_NAME={db_name}")
+    print(f"DB_USER={db_user}")
+    print(f"DB_PASSWORD={db_password}")
+    print(f"DB_HOST={db_host}")
+    print(f"DB_PORT={db_port}")
+
     return SQLAlchemyAdapter(
         db_name = db_name,
         db_path = db_path,
