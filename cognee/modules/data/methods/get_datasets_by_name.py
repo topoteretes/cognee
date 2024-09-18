0b9656b5bb7fbf4df4e782c72b85cd932f0c5193
Vasilije <8619304+Vasilije1990@users.noreply.github.com>
2024-09-18 16:24:32 +0200

    Fix bug with single input

--- cognee/modules/data/methods/get_datasets_by_name.py
+++ cognee/modules/data/methods/get_datasets_by_name.py
@@ -7,6 +7,8 @@ async def get_datasets_by_name(dataset_names: list[str], user_id: UUID) -> list[
     db_engine = get_relational_engine()
 
     async with db_engine.get_async_session() as session:
+        if isinstance(dataset_names, str):
+            dataset_names = [dataset_names]
         datasets = (await session.scalars(
             select(Dataset)
                 .filter(Dataset.owner_id == user_id)
