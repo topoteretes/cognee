09f5fa8ef5a40a3850e40d5f50ae4a74a2fadccd
Igor Ilic <igorilic03@gmail.com>
2024-10-27 22:04:27 +0100

    feat: Add deletion by id for SqlAlchemyAdapter
    Added ability to delete data from database by id
    
    Feature #COG-414

--- cognee/infrastructure/databases/relational/sqlalchemy/SqlAlchemyAdapter.py
+++ cognee/infrastructure/databases/relational/sqlalchemy/SqlAlchemyAdapter.py
@@ -1,8 +1,9 @@
 from os import path
+from uuid import UUID
 from typing import Optional
-from typing import AsyncGenerator
+from typing import AsyncGenerator, List
 from contextlib import asynccontextmanager
-from sqlalchemy import text, select, MetaData
+from sqlalchemy import text, select, MetaData, Table
 from sqlalchemy.orm import joinedload
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 
@@ -69,6 +70,51 @@ class SQLAlchemyAdapter():
             await connection.execute(insert_query, data)
             await connection.close()
 
+    async def get_schema_list(self) -> List[str]:
+        """
+        Return a list of all schema names in database
+        """
+        if self.engine.dialect.name == "postgresql":
+            async with self.engine.begin() as connection:
+                result = await connection.execute(
+                    text("""
+                        SELECT schema_name FROM information_schema.schemata
+                        WHERE schema_name NOT IN ('pg_catalog', 'pg_toast', 'information_schema');
+                        """)
+                )
+                return [schema[0] for schema in result.fetchall()]
+        return []
+
+    async def delete_data_by_id(self, table_name: str, data_id: UUID, schema_name: Optional[str] = "public"):
+        """
+        Delete data in given table based on id. Table must have an id Column.
+        """
+        async with self.get_async_session() as session:
+            TableModel = await self.get_table(table_name, schema_name)
+            await session.execute(TableModel.delete().where(TableModel.c.id == data_id))
+            await session.commit()
+
+    async def get_table(self, table_name: str, schema_name: Optional[str] = "public") -> Table:
+        """
+        Dynamically loads a table using the given table name and schema name.
+        """
+        async with self.engine.begin() as connection:
+            if self.engine.dialect.name == "sqlite":
+                # Load the schema information into the MetaData object
+                await connection.run_sync(Base.metadata.reflect)
+                if table_name in Base.metadata.tables:
+                    return Base.metadata.tables[table_name]
+                else:
+                    raise ValueError(f"Table '{table_name}' not found.")
+            else:
+                # Create a MetaData instance to load table information
+                metadata = MetaData()
+                await connection.run_sync(metadata.reflect, schema=schema_name)
+                if table_name in metadata.tables:
+                    return metadata.tables[table_name]
+                raise ValueError(f"Table '{table_name}' not found.")
+
+
     async def get_data(self, table_name: str, filters: dict = None):
         async with self.engine.begin() as connection:
             query = f"SELECT * FROM {table_name}"
@@ -123,20 +169,15 @@ class SQLAlchemyAdapter():
                 self.db_path = None
             else:
                 async with self.engine.begin() as connection:
-                    result = await connection.execute(
-                        text("""
-                            SELECT schema_name FROM information_schema.schemata
-                            WHERE schema_name NOT IN ('pg_catalog', 'pg_toast', 'information_schema');
-                            """)
-                    )
+                    schema_list = await self.get_schema_list()
                     # Create a MetaData instance to load table information
                     metadata = MetaData()
                     # Drop all tables from all schemas
-                    for schema in result.fetchall():
+                    for schema_name in schema_list:
                         # Load the schema information into the MetaData object
-                        await connection.run_sync(metadata.reflect, schema=schema[0])
+                        await connection.run_sync(metadata.reflect, schema=schema_name)
                         for table in metadata.sorted_tables:
-                            drop_table_query = text(f"DROP TABLE IF EXISTS {schema[0]}.{table.name} CASCADE")
+                            drop_table_query = text(f"DROP TABLE IF EXISTS {schema_name}.{table.name} CASCADE")
                             await connection.execute(drop_table_query)
                         metadata.clear()
         except Exception as e:
