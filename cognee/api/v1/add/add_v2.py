8c7fdc462c2599a77d426a5625abf331c75a1769
Igor Ilic <igorilic03@gmail.com>
2024-11-08 14:55:10 +0100

    refactor: Return previous ingestion Tasks to add function
    Returned previous ignestion tasks to add function
    
    Refactor #COG-337

--- cognee/api/v1/add/add_v2.py
+++ cognee/api/v1/add/add_v2.py
@@ -2,10 +2,9 @@ from typing import Union, BinaryIO
 from cognee.modules.users.models import User
 from cognee.modules.users.methods import get_default_user
 from cognee.modules.pipelines import run_tasks, Task
+from cognee.tasks.ingestion import save_data_to_storage, ingest_data
 from cognee.infrastructure.databases.relational import create_db_and_tables as create_relational_db_and_tables
 from cognee.infrastructure.databases.vector.pgvector import create_db_and_tables as create_pgvector_db_and_tables
-from cognee.tasks.ingestion.ingest_data_with_metadata import ingest_data_with_metadata
-
 
 async def add(data: Union[BinaryIO, list[BinaryIO], str, list[str]], dataset_name: str = "main_dataset", user: User = None):
     await create_relational_db_and_tables()
@@ -15,7 +14,8 @@ async def add(data: Union[BinaryIO, list[BinaryIO], str, list[str]], dataset_nam
         user = await get_default_user()
 
     tasks = [
-        Task(ingest_data_with_metadata, dataset_name, user)
+        Task(save_data_to_storage, dataset_name),
+        Task(ingest_data, dataset_name, user)
     ]
 
     pipeline = run_tasks(tasks, data, "add_pipeline")
