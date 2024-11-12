2be11279967a831c4bae4b9f9d1badeb8a15ba7f
Leon Luithlen <leon@topoteretes.com>
2024-11-12 14:31:26 +0100

    Makedirs for integration test

--- cognee/tests/integration/run_toy_tasks/conftest.py
+++ cognee/tests/integration/run_toy_tasks/conftest.py
@@ -5,4 +5,5 @@ import pytest
 
 @pytest.fixture(autouse=True, scope="session")
 def copy_cognee_db_to_target_location():
+    os.makedirs("cognee/.cognee_system/databases/")
     os.system("cp cognee/tests/integration/run_toy_tasks/data/cognee_db cognee/.cognee_system/databases/cognee_db")
