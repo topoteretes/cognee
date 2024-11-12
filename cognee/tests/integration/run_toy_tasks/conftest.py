e6bdb6703a3bb2bc8e4b129c3e577d4b78b3fcca
Leon Luithlen <leon@topoteretes.com>
2024-11-12 14:16:30 +0100

    cp cognee_db for integration test

--- cognee/tests/integration/run_toy_tasks/conftest.py
+++ cognee/tests/integration/run_toy_tasks/conftest.py
@@ -5,6 +5,4 @@ import pytest
 
 @pytest.fixture(autouse=True, scope="session")
 def copy_cognee_db_to_target_location():
-
-    os.system
-    yield
+    os.system("cp cognee/tests/integration/run_toy_tasks/data/cognee_db cognee/.cognee_system/databases/cognee_db")
--- cognee/tests/unit/run_tasks/conftest.py
+++
@@ -1,7 +0,0 @@
-
-import os
-import pytest
-
-@pytest.fixture(autouse=True, scope="session")
-def copy_cognee_db_to_target_location():
-    os.system("mv cognee/tests/integration/run_toy_tasks/data/cognee_db cognee/.cognee_system/databases")
