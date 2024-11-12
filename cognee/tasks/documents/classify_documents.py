826de0edbf03650109b7e250f729f77934c415e2
Leon Luithlen <leon@topoteretes.com>
2024-11-12 16:35:40 +0100

    Remove orphan dictionary

--- cognee/tasks/documents/classify_documents.py
+++ cognee/tasks/documents/classify_documents.py
@@ -1,13 +1,6 @@
 from cognee.modules.data.models import Data
 from cognee.modules.data.processing.document_types import Document, PdfDocument, AudioDocument, ImageDocument, TextDocument
 
-EXTENSION_TO_DOCUMENT_CLASS = {
-    "pdf": PdfDocument,
-    "audio": AudioDocument,
-    "image": ImageDocument,
-    "txt": TextDocument
-}
-
 def classify_documents(data_documents: list[Data]) -> list[Document]:
     documents = [
         PdfDocument(id = data_item.id, name=f"{data_item.name}.{data_item.extension}", raw_data_location=data_item.raw_data_location) if data_item.extension == "pdf" else
