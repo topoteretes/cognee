cc6fbe2a5fc750b206d763e3ce45f16c9a371b79
Igor Ilic <igorilic03@gmail.com>
2024-12-06 13:48:39 +0100

    refactor: Add space to ingest function
    Add space and newline to ingest function
    
    Refactor COG-505

--- cognee/modules/ingestion/identify.py
+++ cognee/modules/ingestion/identify.py
@@ -3,7 +3,9 @@ from .data_types import IngestionData
 
 from cognee.modules.users.models import User
 
+
 def identify(data: IngestionData, user: User) -> str:
     data_content_hash: str = data.get_identifier()
+
     # return UUID hash of file contents + owner id
-    return uuid5(NAMESPACE_OID,f"{data_content_hash}{user.id}")
+    return uuid5(NAMESPACE_OID, f"{data_content_hash}{user.id}")
