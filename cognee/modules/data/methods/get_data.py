43187e4d63f9d1604ca439a346401faad57b1e27
Igor Ilic <igorilic03@gmail.com>
2024-12-13 13:54:45 +0100

    feat: Add user verification for accessing data
    Verify user has access to data before returning it
    
    Feature COG-656

--- cognee/modules/data/methods/get_data.py
+++ cognee/modules/data/methods/get_data.py
@@ -3,10 +3,11 @@ from typing import Optional
 from cognee.infrastructure.databases.relational import get_relational_engine
 from ..models import Data
 
-async def get_data(data_id: UUID) -> Optional[Data]:
+async def get_data(user_id: UUID, data_id: UUID) -> Optional[Data]:
     """Retrieve data by ID.
 
         Args:
+            user_id (UUID): user ID
             data_id (UUID): ID of the data to retrieve
 
         Returns:
@@ -16,5 +17,8 @@ async def get_data(data_id: UUID) -> Optional[Data]:
 
     async with db_engine.get_async_session() as session:
         data = await session.get(Data, data_id)
+
+        if data and data.owner_id != user_id:
+            return None
 
         return data
