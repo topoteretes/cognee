b976f5b7a62852b07cd0b3f914275c702d556a39
vasilije <vas.markovic@gmail.com>
2024-12-17 20:59:45 +0100

    First draft of relationship embeddings

--- cognee/base_config.py
+++ cognee/base_config.py
@@ -12,7 +12,7 @@ class BaseConfig(BaseSettings):
     graphistry_password: Optional[str] = os.getenv("GRAPHISTRY_PASSWORD")
     langfuse_public_key: Optional[str] = os.getenv("LANGFUSE_PUBLIC_KEY")
     langfuse_secret_key: Optional[str] = os.getenv("LANGFUSE_SECRET_KEY")
-    langfuse_host: Optional[str] = os.environ["LANGFUSE_HOST"]
+    langfuse_host: Optional[str] = os.getenv("LANGFUSE_HOST")
 
     model_config = SettingsConfigDict(env_file = ".env", extra = "allow")
 
