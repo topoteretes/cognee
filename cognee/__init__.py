b13edb0ece3a95e691bd3a3c53f78208d37aead7
vasilije <vas.markovic@gmail.com>
2025-01-08 13:29:23 +0100

    Fix visualization

--- cognee/__init__.py
+++ cognee/__init__.py
@@ -5,7 +5,7 @@ from .api.v1.datasets.datasets import datasets
 from .api.v1.prune import prune
 from .api.v1.search import SearchType, get_search_history, search
 from .api.v1.visualize import visualize
-from .shared.utils import *
+from .shared.utils import create_cognee_style_network_with_logo
 
 # Pipelines
 from .modules import pipelines
