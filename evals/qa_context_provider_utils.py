61230acf55463794b97b18437ee41dd527a52e36
Rita Aleksziev <alekszievr@gmail.com>
2025-01-16 10:30:07 +0100

    enable context provider to get tasks as param

--- evals/qa_context_provider_utils.py
+++ evals/qa_context_provider_utils.py
@@ -3,35 +3,54 @@ from cognee.api.v1.search import SearchType
 from cognee.infrastructure.databases.vector import get_vector_engine
 from cognee.modules.retrieval.brute_force_triplet_search import brute_force_triplet_search
 from cognee.tasks.completion.graph_query_completion import retrieved_edges_to_string
+from cognee.modules.pipelines.tasks.Task import Task
+from functools import partial
 
 
 async def get_raw_context(instance: dict) -> str:
     return instance["context"]
 
 
-async def cognify_instance(instance: dict):
+async def cognify_instance(instance: dict, tasks: list[Task] = None):
     await cognee.prune.prune_data()
     await cognee.prune.prune_system(metadata=True)
 
     for title, sentences in instance["context"]:
         await cognee.add("\n".join(sentences), dataset_name="QA")
-    await cognee.cognify("QA")
+    await cognee.cognify("QA", tasks)
 
 
-async def get_context_with_cognee(instance: dict) -> str:
-    await cognify_instance(instance)
+async def get_context_with_cognee(
+    instance: dict,
+    tasks: list[Task] = None,
+    search_types: list[SearchType] = [SearchType.INSIGHTS, SearchType.SUMMARIES, SearchType.CHUNKS],
+) -> str:
+    await cognify_instance(instance, tasks)
 
-    insights = await cognee.search(SearchType.INSIGHTS, query_text=instance["question"])
-    summaries = await cognee.search(SearchType.SUMMARIES, query_text=instance["question"])
-    search_results = insights + summaries
+    search_results = []
+    for search_type in search_types:
+        search_results += await cognee.search(search_type, query_text=instance["question"])
+
+    # insights = await cognee.search(SearchType.INSIGHTS, query_text=instance["question"])
+    # summaries = await cognee.search(SearchType.SUMMARIES, query_text=instance["question"])
+    # chunks = await cognee.search(SearchType.CHUNKS, query_text=instance["question"])
+    # search_results = insights + summaries + chunks
 
     search_results_str = "\n".join([context_item["text"] for context_item in search_results])
 
     return search_results_str
 
 
+def create_cognee_context_getter(tasks=None, search_types=None):
+    return partial(get_context_with_cognee, tasks=tasks, search_types=search_types)
+
+
 async def get_context_with_simple_rag(instance: dict) -> str:
-    await cognify_instance(instance)
+    await cognee.prune.prune_data()
+    await cognee.prune.prune_system(metadata=True)
+
+    for title, sentences in instance["context"]:
+        await cognee.add("\n".join(sentences), dataset_name="QA")
 
     vector_engine = get_vector_engine()
     found_chunks = await vector_engine.search("document_chunk_text", instance["question"], limit=5)
