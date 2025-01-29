06030ff768f6e214cd5c826ec38c209932e08946
Rita Aleksziev <alekszievr@gmail.com>
2025-01-29 14:28:41 +0100

    Graph metrics getter template in graph db interface and adapters

--- cognee/tasks/storage/descriptive_metrics.py
+++ cognee/tasks/storage/descriptive_metrics.py
@@ -24,22 +24,12 @@ async def fetch_token_count(db_engine) -> int:
     return token_count_sum
 
 
-async def calculate_graph_metrics(graph_data):
-    nodes, edges = graph_data
-    graph_metrics = {
-        "num_nodes": len(nodes),
-        "num_edges": len(edges),
-    }
-    return graph_metrics
-
-
 async def store_descriptive_metrics(data_points: list[DataPoint]):
     db_engine = get_relational_engine()
     graph_engine = await get_graph_engine()
-    graph_data = await graph_engine.get_graph_data()
 
     token_count_sum = await fetch_token_count(db_engine)
-    graph_metrics = await calculate_graph_metrics(graph_data)
+    graph_metrics = await graph_engine.get_graph_metrics()
 
     table_name = "graph_metrics_table"
     metrics_dict = {"id": uuid.uuid4(), "num_tokens": token_count_sum} | graph_metrics
