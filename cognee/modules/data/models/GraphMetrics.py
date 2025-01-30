a79f7133fddb375fc733c0144b9f1205d5b694df
alekszievr <44192193+alekszievr@users.noreply.github.com>
2025-01-30 12:39:14 +0100

    Feat: add number of tokens and descriptive graph metrics to metric table [COG-1132] (#481)
    * Count the number of tokens in documents
    
    * save token count to relational db
    
    * Add metrics to metric table
    
    * Store list as json instead of array in relational db table
    
    * Sum in sql instead of python
    
    * Unify naming
    
    * Return data_points in descriptive metric calculation task
    
    ---------
    
    Co-authored-by: Igor Ilic <30923996+dexters1@users.noreply.github.com>

--- cognee/modules/data/models/GraphMetrics.py
+++ cognee/modules/data/models/GraphMetrics.py
@@ -1,23 +1,23 @@
 from datetime import datetime, timezone
 
-from sqlalchemy import Column, DateTime, Float, Integer, ARRAY, UUID
+from sqlalchemy import Column, DateTime, Float, Integer, JSON, UUID
 
 from cognee.infrastructure.databases.relational import Base
 from uuid import uuid4
 
 
-class GraphMetricData(Base):
-    __tablename__ = "graph_metric_table"
+class GraphMetrics(Base):
+    __tablename__ = "graph_metrics_table"
 
     # TODO: Change ID to reflect unique id of graph database
     id = Column(UUID, primary_key=True, default=uuid4)
-    num_tokens = Column(Integer)
-    num_nodes = Column(Integer)
-    num_edges = Column(Integer)
-    mean_degree = Column(Float)
-    edge_density = Column(Float)
-    num_connected_components = Column(Integer)
-    sizes_of_connected_components = Column(ARRAY(Integer))
+    num_tokens = Column(Integer, nullable=True)
+    num_nodes = Column(Integer, nullable=True)
+    num_edges = Column(Integer, nullable=True)
+    mean_degree = Column(Float, nullable=True)
+    edge_density = Column(Float, nullable=True)
+    num_connected_components = Column(Integer, nullable=True)
+    sizes_of_connected_components = Column(JSON, nullable=True)
     num_selfloops = Column(Integer, nullable=True)
     diameter = Column(Integer, nullable=True)
     avg_shortest_path_length = Column(Float, nullable=True)
