268d7782e6e210773c5fe4c64c7000b4f15cf1c0
Rita Aleksziev <alekszievr@gmail.com>
2025-02-03 11:35:27 +0100

    Use modules for adding to db instead of infrastructure

--- cognee/modules/data/methods/store_descriptive_metrics.py
+++ cognee/modules/data/methods/store_descriptive_metrics.py
@@ -1,5 +1,4 @@
 from cognee.infrastructure.engine import DataPoint
-from cognee.modules.data.processing.document_types import Document
 from cognee.infrastructure.databases.relational import get_relational_engine
 from sqlalchemy import select
 from sqlalchemy.sql import func
@@ -27,12 +26,25 @@ async def fetch_token_count(db_engine) -> int:
 async def store_descriptive_metrics(data_points: list[DataPoint]):
     db_engine = get_relational_engine()
     graph_engine = await get_graph_engine()
-
-    token_count_sum = await fetch_token_count(db_engine)
     graph_metrics = await graph_engine.get_graph_metrics()
 
-    table_name = "graph_metrics"
-    metrics_dict = {"id": uuid.uuid4(), "num_tokens": token_count_sum} | graph_metrics
+    async with db_engine.get_async_session() as session:
+        metrics = GraphMetrics(
+            id=uuid.uuid4(),
+            num_tokens=await fetch_token_count(db_engine),
+            num_nodes=graph_metrics["num_nodes"],
+            num_edges=graph_metrics["num_edges"],
+            mean_degree=graph_metrics["mean_degree"],
+            edge_density=graph_metrics["edge_density"],
+            num_connected_components=graph_metrics["num_connected_components"],
+            sizes_of_connected_components=graph_metrics["sizes_of_connected_components"],
+            num_selfloops=graph_metrics["num_selfloops"],
+            diameter=graph_metrics["diameter"],
+            avg_shortest_path_length=graph_metrics["avg_shortest_path_length"],
+            avg_clustering=graph_metrics["avg_clustering"],
+        )
 
-    await db_engine.insert_data(table_name, metrics_dict)
+        session.add(metrics)
+        await session.commit()
+
     return data_points
--- cognee/modules/data/models/__init__.py
+++ cognee/modules/data/models/__init__.py
@@ -1,3 +1,4 @@
 from .Data import Data
 from .Dataset import Dataset
 from .DatasetData import DatasetData
+from .GraphMetrics import GraphMetrics
