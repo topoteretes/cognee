bcd326518d8a1c0c565ff4469847b70f2d95d054
hajdul88 <52442977+hajdul88@users.noreply.github.com>
2025-02-06 11:22:17 +0100

    feat: implements graph visualization method for cognee (#493)
    <!-- .github/pull_request_template.md -->
    
    ## Description
    This PR contains the improvement of the visualization endpoint
    
    ## DCO Affirmation
    I affirm that all code in every commit of this pull request conforms to
    the terms of the Topoteretes Developer Certificate of Origin
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Launched an enhanced interactive network visualization utility that
    renders dynamic, browser-based graphs. The new feature simplifies
    execution by directly generating an HTML file showcasing the
    visualization—complete with interactive elements and an on-screen
    confirmation—providing a more intuitive and efficient experience.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- cognee/api/v1/visualize/visualize.py
+++ cognee/api/v1/visualize/visualize.py
@@ -1,15 +1,30 @@
-from cognee.shared.utils import create_cognee_style_network_with_logo, graph_to_tuple
+from cognee.modules.visualization.cognee_network_visualization import (
+    cognee_network_visualization,
+)
 from cognee.infrastructure.databases.graph import get_graph_engine
 import logging
 
 
-async def visualize_graph(label: str = "name"):
-    """ """
+import asyncio
+from cognee.shared.utils import setup_logging
+
+
+async def visualize_graph():
     graph_engine = await get_graph_engine()
     graph_data = await graph_engine.get_graph_data()
     logging.info(graph_data)
 
-    graph = await create_cognee_style_network_with_logo(graph_data, label=label)
+    graph = await cognee_network_visualization(graph_data)
     logging.info("The HTML file has been stored on your home directory! Navigate there with cd ~")
 
     return graph
+
+
+if __name__ == "__main__":
+    setup_logging(logging.ERROR)
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        loop.run_until_complete(visualize_graph())
+    finally:
+        loop.run_until_complete(loop.shutdown_asyncgens())
