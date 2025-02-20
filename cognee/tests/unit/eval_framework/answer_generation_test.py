7baa8c5391743e8e23123cdb2a75931edcd73804
Rita Aleksziev <alekszievr@gmail.com>
2025-02-20 15:27:04 +0100

    Mock cognee in answer generation test

--- cognee/tests/unit/eval_framework/answer_generation_test.py
+++ cognee/tests/unit/eval_framework/answer_generation_test.py
@@ -4,7 +4,7 @@ from evals.eval_framework.answer_generation.answer_generation_executor import (
     question_answering_engine_options,
 )
 from evals.eval_framework.benchmark_adapters.dummy_adapter import DummyAdapter
-import cognee
+from unittest.mock import AsyncMock
 
 
 qa_engine_options = [
@@ -12,21 +12,18 @@ qa_engine_options = [
 ]
 
 
-@pytest.mark.parametrize("qa_engine", qa_engine_options)
 @pytest.mark.asyncio
-async def test_answer_generation(qa_engine):
+async def test_answer_generation():
     limit = 1
     corpus_list, qa_pairs = DummyAdapter().load_corpus(limit=limit)
 
-    await cognee.prune.prune_data()
-    await cognee.prune.prune_system(metadata=True)
-    await cognee.add(corpus_list)
-    await cognee.cognify()
+    mock_answer_resolver = AsyncMock()
+    mock_answer_resolver.side_effect = lambda query: "mock_answer"
 
     answer_generator = AnswerGeneratorExecutor()
     answers = await answer_generator.question_answering_non_parallel(
         questions=qa_pairs,
-        answer_resolver=qa_engine,
+        answer_resolver=mock_answer_resolver,
     )
     assert len(answers) == len(qa_pairs)
     assert answers[0]["question"] == qa_pairs[0]["question"], (
