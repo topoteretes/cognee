931717e67542a6c35eb0022c539e492a3da15132
Daniel Molnar <soobrosa@gmail.com>
2025-02-27 14:44:17 +0100

    Lean inits.

--- cognee/tasks/summarization/__init__.py
+++ cognee/tasks/summarization/__init__.py
@@ -1,11 +1,2 @@
-from cognee.modules.retrieval.summaries_retriever import SummariesRetriever
 from .summarize_code import summarize_code
 from .summarize_text import summarize_text
-
-# Instantiate retriever
-summaries_retriever = SummariesRetriever()
-
-
-# Define async function to expose retrieval functionality
-async def query_summaries(query: str):
-    return await summaries_retriever.get_completion(query)
