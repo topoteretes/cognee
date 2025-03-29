738fc9ba8e53d7f5f7cfbbe3cf7ff18e2a097694
Boris Arzentar <borisarzentar@gmail.com>
2025-03-29 19:14:16 +0100

    feat: add LoggerInterface

--- cognee/shared/logging_utils.py
+++ cognee/shared/logging_utils.py
@@ -117,7 +117,24 @@ class PlainFileHandler(logging.FileHandler):
             self.flush()
 
 
-def get_logger(name=None, level=None):
+class LoggerInterface:
+    def info(self, msg, *args, **kwargs):
+        pass
+
+    def warning(self, msg, *args, **kwargs):
+        pass
+
+    def error(self, msg, *args, **kwargs):
+        pass
+
+    def critical(self, msg, *args, **kwargs):
+        pass
+
+    def debug(self, msg, *args, **kwargs):
+        pass
+
+
+def get_logger(name=None, level=None) -> LoggerInterface:
     """Get a configured structlog logger.
 
     Args:
