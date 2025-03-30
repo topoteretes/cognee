f8b6f1b18641429c4976954a106190518bc1dcaa
vasilije <vas.markovic@gmail.com>
2025-03-30 18:16:27 +0200

    Add default user

--- cognee/modules/users/methods/get_default_user.py
+++ cognee/modules/users/methods/get_default_user.py
@@ -4,16 +4,19 @@ from sqlalchemy.future import select
 from cognee.modules.users.models import User
 from cognee.infrastructure.databases.relational import get_relational_engine
 from cognee.modules.users.methods.create_default_user import create_default_user
+from cognee.base_config import get_base_config
 
 
 async def get_default_user() -> SimpleNamespace:
     db_engine = get_relational_engine()
+    base_config = get_base_config()
+    default_email = base_config.default_user_email or "default_user@example.com"
 
     async with db_engine.get_async_session() as session:
         query = (
             select(User)
             .options(selectinload(User.roles))
-            .where(User.email == "default_user@example.com")
+            .where(User.email == default_email)
         )
 
         result = await session.execute(query)
