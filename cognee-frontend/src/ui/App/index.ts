c62c2c14335a2ab1414e84130c8acb26825b7246
Boris Arzentar <borisarzentar@gmail.com>
2025-04-14 22:10:25 +0200

    fix: cognee ui with new visualization

--- cognee-frontend/src/ui/App/index.ts
+++ cognee-frontend/src/ui/App/index.ts
@@ -1,4 +1,3 @@
-export { default as Logo } from './Logo/Logo';
-export { default as TextLogo } from './Logo/TextLogo.v2';
+export { default as TextLogo } from './Logo/TextLogo';
 export { default as LoadingIndicator } from './Loading/DefaultLoadingIndicator/LoadingIndicator';
 export { default as CognifyLoadingIndicator } from './Loading/CognifyLoadingIndicator/CognifyLoadingIndicator';
