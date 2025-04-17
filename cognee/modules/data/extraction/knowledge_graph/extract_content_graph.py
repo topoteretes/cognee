5071e8ec1d81d1f7f1435b14930712b3c92fa17d
arthurevain <arthur.evain@telecom-paris.fr>
2025-04-17 15:59:12 +0200

    Fix issue 737
    Signed-off-by: arthurevain <arthur.evain@telecom-paris.fr>

--- cognee/modules/data/extraction/knowledge_graph/extract_content_graph.py
+++ cognee/modules/data/extraction/knowledge_graph/extract_content_graph.py
@@ -2,12 +2,22 @@ from typing import Type
 from pydantic import BaseModel
 from cognee.infrastructure.llm.get_llm_client import get_llm_client
 from cognee.infrastructure.llm.prompts import render_prompt
+from dotenv import load_dotenv
+import os
 
 
 async def extract_content_graph(content: str, response_model: Type[BaseModel]):
     llm_client = get_llm_client()
+    
+    load_dotenv()
+    graph_prompt_path = os.getenv("GRAPH_PROMPT_PATH")
 
-    system_prompt = render_prompt("generate_graph_prompt.txt", {})
+    if graph_prompt_path is not None:
+        path = graph_prompt_path
+    else:
+        path = "generate_graph_prompt.txt"
+
+    system_prompt = render_prompt(path, {})
     content_graph = await llm_client.acreate_structured_output(
         content, system_prompt, response_model
     )
