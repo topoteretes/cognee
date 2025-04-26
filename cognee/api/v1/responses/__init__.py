2018850dff8cc4437b151aa59d45583ff6e56522
Dmitrii Galkin <galkin.dmitry.485@gmail.com>
2025-04-26 20:14:35 +0400

    Add OpenAI-compatible chat and responses API endpoints with function calling support

---
+++ cognee/api/v1/responses/__init__.py
@@ -0,0 +1,3 @@
+from cognee.api.v1.responses.routers import get_responses_router
+
+__all__ = ["get_responses_router"]
---
+++ cognee/api/v1/responses/default_tools.py
@@ -0,0 +1,66 @@
+DEFAULT_TOOLS = [
+    {
+        "type": "function",
+        "name": "search",
+        "description": "Search for information within the knowledge graph",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "search_query": {
+                    "type": "string",
+                    "description": "The query to search for in the knowledge graph",
+                },
+                "search_type": {
+                    "type": "string",
+                    "description": "Type of search to perform",
+                    "enum": [
+                        "INSIGHTS",
+                        "CODE",
+                        "GRAPH_COMPLETION",
+                        "SEMANTIC",
+                        "NATURAL_LANGUAGE",
+                    ],
+                },
+                "top_k": {
+                    "type": "integer",
+                    "description": "Maximum number of results to return",
+                    "default": 10,
+                },
+                "datasets": {
+                    "type": "array",
+                    "items": {"type": "string"},
+                    "description": "Optional list of dataset names to search within",
+                },
+            },
+            "required": ["search_query"],
+        },
+    },
+    {
+        "type": "function",
+        "name": "cognify_text",
+        "description": "Convert text into a knowledge graph or process all added content",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "text": {
+                    "type": "string",
+                    "description": "Text content to be converted into a knowledge graph",
+                },
+                "graph_model_name": {
+                    "type": "string",
+                    "description": "Name of the graph model to use",
+                },
+                "graph_model_file": {
+                    "type": "string",
+                    "description": "Path to a custom graph model file",
+                },
+            },
+        },
+    },
+    # Commented as dangerous
+    # {
+    #     "type": "function",
+    #     "name": "prune",
+    #     "description": "Prune memory",
+    # },
+]
