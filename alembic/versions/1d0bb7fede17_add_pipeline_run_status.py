92389f42554cd1c07ce714ee885f281976c82660
hajdul88 <52442977+hajdul88@users.noreply.github.com>
2025-05-19 12:54:23 +0200

    Fix: Migration fix for pipeline run status enum + fixes default user exist case

--- alembic/versions/1d0bb7fede17_add_pipeline_run_status.py
+++ alembic/versions/1d0bb7fede17_add_pipeline_run_status.py
@@ -1,9 +1,8 @@
-"""Add  pipeline run status
+"""Add pipeline run status
 
 Revision ID: 1d0bb7fede17
 Revises: 482cd6517ce4
 Create Date: 2025-05-19 10:58:15.993314
-
 """
 
 from typing import Sequence, Union
@@ -18,21 +17,16 @@ from cognee.modules.pipelines.models.PipelineRun import PipelineRun, PipelineRun
 revision: str = "1d0bb7fede17"
 down_revision: Union[str, None] = "482cd6517ce4"
 branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = "482cd6517ce4"
 
 
-async def upgrade() -> None:
+def upgrade() -> None:
     db_engine = get_relational_engine()
 
     if db_engine.engine.dialect.name == "postgresql":
         op.execute(
-            f"ALTER TYPE {PipelineRun.status} ADD VALUE IF NOT EXISTS '{str(PipelineRunStatus.DATASET_PROCESSING_INITIATED)}'"
+            "ALTER TYPE pipelinerunstatus ADD VALUE IF NOT EXISTS 'DATASET_PROCESSING_INITIATED'"
         )
-    else:
-        op.execute(f"""
-            ALTER TABLE {PipelineRun}
-            MODIFY COLUMN {PipelineRun.status} ENUM('{PipelineRunStatus.DATASET_PROCESSING_INITIATED}', '{PipelineRunStatus.DATASET_PROCESSING_STARTED}', '{PipelineRunStatus.DATASET_PROCESSING_COMPLETED}', '{PipelineRunStatus.DATASET_PROCESSING_ERRORED}') NOT NULL
-        """)
 
 
 def downgrade() -> None:
