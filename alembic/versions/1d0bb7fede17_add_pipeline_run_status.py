c8a7a6709f07902377235558103da39381952ab1
Boris Arzentar <borisarzentar@gmail.com>
2025-05-19 11:18:15 +0200

    fix: ruff lint errors

--- alembic/versions/1d0bb7fede17_add_pipeline_run_status.py
+++ alembic/versions/1d0bb7fede17_add_pipeline_run_status.py
@@ -5,6 +5,7 @@ Revises: 482cd6517ce4
 Create Date: 2025-05-19 10:58:15.993314
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -24,12 +25,15 @@ async def upgrade() -> None:
     db_engine = get_relational_engine()
 
     if db_engine.engine.dialect.name == "postgresql":
-        op.execute(f"ALTER TYPE {PipelineRun.status} ADD VALUE IF NOT EXISTS '{str(PipelineRunStatus.DATASET_PROCESSING_INITIATED)}'")
+        op.execute(
+            f"ALTER TYPE {PipelineRun.status} ADD VALUE IF NOT EXISTS '{str(PipelineRunStatus.DATASET_PROCESSING_INITIATED)}'"
+        )
     else:
         op.execute(f"""
             ALTER TABLE {PipelineRun}
             MODIFY COLUMN {PipelineRun.status} ENUM('{PipelineRunStatus.DATASET_PROCESSING_INITIATED}', '{PipelineRunStatus.DATASET_PROCESSING_STARTED}', '{PipelineRunStatus.DATASET_PROCESSING_COMPLETED}', '{PipelineRunStatus.DATASET_PROCESSING_ERRORED}') NOT NULL
         """)
+
 
 def downgrade() -> None:
     pass
