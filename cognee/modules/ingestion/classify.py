f0847d6d5288dff5501a4e0bebad2d1b1ffe4de5
hajdul88 <52442977+hajdul88@users.noreply.github.com>
2025-06-12 12:15:48 +0200

    fix: changes and repositions s3

--- cognee/modules/ingestion/classify.py
+++ cognee/modules/ingestion/classify.py
@@ -5,8 +5,21 @@ from tempfile import SpooledTemporaryFile
 
 from cognee.modules.ingestion.exceptions import IngestionError
 
+try:
+    from s3fs.core import S3File
+    from cognee.modules.ingestion.data_types.S3BinaryData import S3BinaryData
+except ImportError:
+    S3File = None
+    S3BinaryData = None
+
 
 def classify(data: Union[str, BinaryIO], filename: str = None, s3fs: Optional[Any] = None):
+    if isinstance(data, str):
+        return TextData(data)
+
+    if isinstance(data, BufferedReader) or isinstance(data, SpooledTemporaryFile):
+        return BinaryData(data, str(data.name).split("/")[-1] if data.name else filename)
+
     try:
         from importlib import import_module
 
@@ -16,17 +29,11 @@ def classify(data: Union[str, BinaryIO], filename: str = None, s3fs: Optional[An
         S3File = None
 
     if S3File is not None:
-        from .data_types import S3BinaryData
+        from cognee.modules.ingestion.data_types.S3BinaryData import S3BinaryData
 
         if isinstance(data, S3File):
             derived_filename = str(data.full_name).split("/")[-1] if data.full_name else filename
             return S3BinaryData(s3_path=data.full_name, name=derived_filename, s3=s3fs)
-
-    if isinstance(data, str):
-        return TextData(data)
-
-    if isinstance(data, BufferedReader) or isinstance(data, SpooledTemporaryFile):
-        return BinaryData(data, str(data.name).split("/")[-1] if data.name else filename)
 
     raise IngestionError(
         message=f"Type of data sent to classify(data: Union[str, BinaryIO) not supported or s3fs is not installed: {type(data)}"
