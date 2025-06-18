008c0cfd0aaaa7ce2d32cf434daba1a8b2fa6eaf
Boris Arzentar <borisarzentar@gmail.com>
2025-06-18 16:37:46 +0200

    fix: run cognify blocking from UI

--- cognee-frontend/src/modules/datasets/cognifyDataset.ts
+++ cognee-frontend/src/modules/datasets/cognifyDataset.ts
@@ -1,5 +1,5 @@
 import { fetch } from "@/utils";
-// import getDatasetGraph from "./getDatasetGraph";
+import getDatasetGraph from "./getDatasetGraph";
 import { Dataset } from "../ingestion/useDatasets";
 
 interface GraphData {
@@ -8,57 +8,51 @@ interface GraphData {
 }
 
 export default async function cognifyDataset(dataset: Dataset, onUpdate: (data: GraphData) => void) {
-  const data = await (
-    fetch("/v1/cognify", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        datasetIds: [dataset.id],
-        runInBackground: true,
-      }),
-    })
-    .then((response) => response.json())
-  );
-    // .then(() => {
-    //   return getDatasetGraph(dataset)
-    //     .then((data) => {
-    //       onUpdate({
-    //         nodes: data.nodes,
-    //         edges: data.edges,
-    //       });
-    //     });
-    // });
+  // const data = await (
+  return fetch("/v1/cognify", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      datasetIds: [dataset.id],
+      runInBackground: false,
+    }),
+  })
+  .then((response) => response.json())
+  .then(() => {
+    return getDatasetGraph(dataset)
+      .then((data) => {
+        onUpdate({
+          nodes: data.nodes,
+          edges: data.edges,
+        });
+      });
+  });
+  // )
 
-    const websocket = new WebSocket(`ws://localhost:8000/api/v1/cognify/subscribe/${data.pipeline_run_id}`);
+    // const websocket = new WebSocket(`ws://localhost:8000/api/v1/cognify/subscribe/${data.pipeline_run_id}`);
 
-    // websocket.onopen = () => {
-    //   websocket.send(JSON.stringify({
-    //     "Authorization": `Bearer ${localStorage.getItem("access_token")}`,
-    //   }));
+    // let isCognifyDone = false;
+
+    // websocket.onmessage = (event) => {
+    //   const data = JSON.parse(event.data);
+    //   onUpdate?.({
+    //     nodes: data.payload.nodes,
+    //     edges: data.payload.edges,
+    //   });
+
+    //   if (data.status === "PipelineRunCompleted") {
+    //     isCognifyDone = true;
+    //     websocket.close();
+    //   }
     // };
 
-    let isCognifyDone = false;
+    // return new Promise(async (resolve) => {
+    //   while (!isCognifyDone) {
+    //     await new Promise(resolve => setTimeout(resolve, 1000));
+    //   }
 
-    websocket.onmessage = (event) => {
-      const data = JSON.parse(event.data);
-      onUpdate?.({
-        nodes: data.payload.nodes,
-        edges: data.payload.edges,
-      });
-
-      if (data.status === "PipelineRunCompleted") {
-        isCognifyDone = true;
-        websocket.close();
-      }
-    };
-
-    return new Promise(async (resolve) => {
-      while (!isCognifyDone) {
-        await new Promise(resolve => setTimeout(resolve, 1000));
-      }
-
-      resolve(true);
-    });
+    //   resolve(true);
+    // });
 }
