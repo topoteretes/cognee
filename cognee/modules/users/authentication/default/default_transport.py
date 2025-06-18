e562bebc053456be7a27e5bbea54ea6d4fe25ccc
Boris Arzentar <borisarzentar@gmail.com>
2025-06-18 11:03:47 +0200

    fix: auth issue with secure cookie

--- cognee/modules/users/authentication/default/default_transport.py
+++ cognee/modules/users/authentication/default/default_transport.py
@@ -3,8 +3,10 @@ from fastapi_users.authentication import CookieTransport
 
 default_transport = CookieTransport(
     cookie_name=os.getenv("AUTH_TOKEN_COOKIE_NAME", "auth_token"),
+    cookie_secure=False,
     cookie_httponly=True,
     cookie_samesite="Lax",
+    cookie_domain="localhost",
 )
 
 default_transport.name = "cookie"
