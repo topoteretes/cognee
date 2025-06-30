14be2a5f5db9c47a92e6969f405dfbba579f4972
Igor Ilic <30923996+dexters1@users.noreply.github.com>
2025-06-30 11:53:17 +0200

    feat: Add dataset_id to pipeline run info and status (#1009)
    <!-- .github/pull_request_template.md -->
    
    ## Description
    <!-- Provide a clear description of the changes in this PR -->
    
    ## DCO Affirmation
    I affirm that all code in every commit of this pull request conforms to
    the terms of the Topoteretes Developer Certificate of Origin.

--- cognee/modules/pipelines/models/PipelineRunInfo.py
+++ cognee/modules/pipelines/models/PipelineRunInfo.py
@@ -6,6 +6,8 @@ from pydantic import BaseModel
 class PipelineRunInfo(BaseModel):
     status: str
     pipeline_run_id: UUID
+    dataset_id: UUID
+    dataset_name: str
     payload: Optional[Any] = None
 
     model_config = {
