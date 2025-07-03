7664ddc748c41a020dcffd72b8c6242bf26522e0
hajdul88 <52442977+hajdul88@users.noreply.github.com>
2025-07-03 18:12:31 +0200

    fix: adds graceful handling of damaged pdf files

--- cognee/modules/data/processing/document_types/PdfDocument.py
+++ cognee/modules/data/processing/document_types/PdfDocument.py
@@ -2,6 +2,9 @@ from pypdf import PdfReader
 from cognee.modules.chunking.Chunker import Chunker
 from .open_data_file import open_data_file
 from .Document import Document
+from cognee.shared.logging_utils import get_logger
+
+logger = get_logger("PDFDocument")
 
 
 class PdfDocument(Document):
@@ -9,12 +12,25 @@ class PdfDocument(Document):
 
     def read(self, chunker_cls: Chunker, max_chunk_size: int):
         with open_data_file(self.raw_data_location, mode="rb") as stream:
-            file = PdfReader(stream)
+            logger.info(f"Reading PDF:{self.raw_data_location}")
+            try:
+                file = PdfReader(stream, strict=False)
+            except Exception as e:
+                logger.warning(
+                    f"PyPDF couldn’t open PDF—skipping: {self.raw_data_location} with error: {e}"  # TODO: Once incremental pipeline implementation is done this has to save pdf errored
+                )
+                return
 
             def get_text():
-                for page in file.pages:
-                    page_text = page.extract_text()
-                    yield page_text
+                try:
+                    for page in file.pages:
+                        page_text = page.extract_text()
+                        yield page_text
+                except Exception as e:
+                    logger.warning(
+                        f"PyPDF couldn’t open PDF—skipping: {self.raw_data_location} with error: {e}"  # TODO: Once incremental pipeline implementation is done this has to save pdf errored
+                    )
+                    return
 
             chunker = chunker_cls(self, get_text=get_text, max_chunk_size=max_chunk_size)
 
