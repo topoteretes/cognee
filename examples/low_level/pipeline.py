61721b9639974040b786e138e709815771bf2b7e
Igor Ilic <igorilic03@gmail.com>
2025-08-05 13:18:11 +0200

    fix: resolve issue with low level pipeline

--- examples/low_level/pipeline.py
+++ examples/low_level/pipeline.py
@@ -13,57 +13,65 @@ from cognee.tasks.storage import add_data_points
 
 class Person(DataPoint):
     name: str
+    # Metadata "index_fields" specifies which DataPoint fields should be embedded for vector search
+    metadata: dict = {"index_fields": ["name"]}
 
 
 class Department(DataPoint):
     name: str
     employees: list[Person]
+    # Metadata "index_fields" specifies which DataPoint fields should be embedded for vector search
+    metadata: dict = {"index_fields": ["name"]}
 
 
 class CompanyType(DataPoint):
     name: str = "Company"
+    # Metadata "index_fields" specifies which DataPoint fields should be embedded for vector search
+    metadata: dict = {"index_fields": ["name"]}
 
 
 class Company(DataPoint):
     name: str
     departments: list[Department]
     is_type: CompanyType
+    # Metadata "index_fields" specifies which DataPoint fields should be embedded for vector search
+    metadata: dict = {"index_fields": ["name"]}
 
 
 def ingest_files(data: List[Any]):
-    people = data[0]["people"]
-    companies = data[0]["companies"]
-
     people_data_points = {}
     departments_data_points = {}
-
-    for person in people:
-        new_person = Person(name=person["name"])
-        people_data_points[person["name"]] = new_person
-
-        if person["department"] not in departments_data_points:
-            departments_data_points[person["department"]] = Department(
-                name=person["department"], employees=[new_person]
-            )
-        else:
-            departments_data_points[person["department"]].employees.append(new_person)
-
     companies_data_points = {}
 
-    # Create a single CompanyType node, so we connect all companies to it.
-    companyType = CompanyType()
+    for data_item in data:
+        people = data_item["people"]
+        companies = data_item["companies"]
 
-    for company in companies:
-        new_company = Company(name=company["name"], departments=[], is_type=companyType)
-        companies_data_points[company["name"]] = new_company
+        for person in people:
+            new_person = Person(name=person["name"])
+            people_data_points[person["name"]] = new_person
 
-        for department_name in company["departments"]:
-            if department_name not in departments_data_points:
-                departments_data_points[department_name] = Department(
-                    name=department_name, employees=[]
+            if person["department"] not in departments_data_points:
+                departments_data_points[person["department"]] = Department(
+                    name=person["department"], employees=[new_person]
                 )
+            else:
+                departments_data_points[person["department"]].employees.append(new_person)
 
-            new_company.departments.append(departments_data_points[department_name])
+        # Create a single CompanyType node, so we connect all companies to it.
+        companyType = CompanyType()
+
+        for company in companies:
+            new_company = Company(name=company["name"], departments=[], is_type=companyType)
+            companies_data_points[company["name"]] = new_company
+
+            for department_name in company["departments"]:
+                if department_name not in departments_data_points:
+                    departments_data_points[department_name] = Department(
+                        name=department_name, employees=[]
+                    )
+
+                new_company.departments.append(departments_data_points[department_name])
 
     return companies_data_points.values()
 
@@ -87,10 +95,13 @@ async def main():
     people_file_path = os.path.join(os.path.dirname(__file__), "people.json")
     people = json.loads(open(people_file_path, "r").read())
 
+    # Run tasks expects a list of data even if it is just one document
+    data = [{"companies": companies, "people": people}]
+
     pipeline = run_tasks(
         [Task(ingest_files), Task(add_data_points)],
         dataset_id=datasets[0].id,
-        data=[{"companies": companies, "people": people}],
+        data=data,
         incremental_loading=False,
     )
 
