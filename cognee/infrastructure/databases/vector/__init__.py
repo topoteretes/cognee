b888648802c89d1b5bc909aa98af7dab58411654
Igor Ilic <igorilic03@gmail.com>
2025-08-11 15:36:16 +0200

    fix: remove unused import

--- cognee/infrastructure/databases/vector/__init__.py
+++ cognee/infrastructure/databases/vector/__init__.py
@@ -4,4 +4,3 @@ from .vector_db_interface import VectorDBInterface
 from .config import get_vectordb_config
 from .get_vector_engine import get_vector_engine
 from .use_vector_adapter import use_vector_adapter
-from .vector_db_async_lock import VECTOR_DB_LOCK
