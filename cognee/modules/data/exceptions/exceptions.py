6870bba5a90285e87f066095a3a878865c343fad
hajdul88 <52442977+hajdul88@users.noreply.github.com>
2025-08-13 12:03:18 +0200

    feat: adds new error to delete

--- cognee/modules/data/exceptions/exceptions.py
+++ cognee/modules/data/exceptions/exceptions.py
@@ -43,3 +43,13 @@ class DatasetTypeError(CogneeValidationError):
         status_code=status.HTTP_400_BAD_REQUEST,
     ):
         super().__init__(message, name, status_code)
+
+
+class InvalidAttributeError(CogneeValidationError):
+    def __init__(
+        self,
+        message: str = "The provided data object is missing the required '__tablename__' attribute.",
+        name: str = "InvalidAttributeError",
+        status_code: int = status.HTTP_400_BAD_REQUEST,
+    ):
+        super().__init__(message, name, status_code)
