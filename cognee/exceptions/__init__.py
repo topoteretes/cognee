0555dd9ddaf48ceec746e65a09adbc748828810c
hajdul88 <52442977+hajdul88@users.noreply.github.com>
2025-08-13 11:29:16 +0200

    Update __init__.py

--- cognee/exceptions/__init__.py
+++ cognee/exceptions/__init__.py
@@ -2,7 +2,7 @@
 Custom exceptions for the Cognee API.
 
 This module defines a set of exceptions for handling various application errors,
-such as service failures, resource conflicts, and invalid operations.
+such as System, Validation, Configuration or TransientErrors
 """
 
 from .exceptions import (
@@ -10,4 +10,5 @@ from .exceptions import (
     CogneeSystemError,
     CogneeValidationError,
     CogneeConfigurationError,
+    CogneeTransientError,
 )
