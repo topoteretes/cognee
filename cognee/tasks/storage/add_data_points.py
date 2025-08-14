63d071f0d86d54246e85eb112f24aea7263db86c
hajdul88 <52442977+hajdul88@users.noreply.github.com>
2025-08-14 14:17:13 +0200

    feat: adds input checks for add datapoints and summarization tasks

--- cognee/tasks/storage/add_data_points.py
+++ cognee/tasks/storage/add_data_points.py
@@ -5,9 +5,18 @@ from cognee.infrastructure.databases.graph import get_graph_engine
 from cognee.modules.graph.utils import deduplicate_nodes_and_edges, get_graph_from_model
 from .index_data_points import index_data_points
 from .index_graph_edges import index_graph_edges
+from cognee.tasks.storage.exceptions import (
+    InvalidDataPointsInAddDataPointsError,
+)
 
 
 async def add_data_points(data_points: List[DataPoint]) -> List[DataPoint]:
+
+    if not isinstance(data_points, list):
+        raise InvalidDataPointsInAddDataPointsError("data_points must be a list.")
+    if not all(isinstance(dp, DataPoint) for dp in data_points):
+        raise InvalidDataPointsInAddDataPointsError("data_points: each item must be a DataPoint.")
+
     nodes = []
     edges = []
 
