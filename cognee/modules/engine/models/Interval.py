bf34ba398e1d3dd39373a0e3b86f0c90e54ef8f7
hajdul88 <52442977+hajdul88@users.noreply.github.com>
2025-08-27 15:14:46 +0200

    feat: adds temporal models for llm extraction

---
+++ cognee/modules/engine/models/Interval.py
@@ -0,0 +1,7 @@
+from pydantic import Field
+from cognee.infrastructure.engine import DataPoint
+from cognee.modules.engine.models.Timestamp import Timestamp
+
+class Interval(DataPoint):
+    time_from: Timestamp = Field(...)
+    time_to: Timestamp = Field(...)
