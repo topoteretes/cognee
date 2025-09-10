e2ed2793140a6993d500f823bf656f5ba93af6cd
Mohammad <mohammad.reshadati@postnl.nl>
2025-09-10 14:14:22 +0200

    feat: add support for AWS session token in S3 configuration

--- cognee/infrastructure/files/storage/s3_config.py
+++ cognee/infrastructure/files/storage/s3_config.py
@@ -11,6 +11,7 @@ class S3Config(BaseSettings):
     aws_session_token: Optional[str] = None
     model_config = SettingsConfigDict(env_file=".env", extra="allow")
 
+
 @lru_cache
 def get_s3_config():
     return S3Config()
