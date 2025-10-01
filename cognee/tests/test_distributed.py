3f70c4d40d151e19b7cf91ebc1ea12b01bd48aee
hajdul88 <52442977+hajdul88@users.noreply.github.com>
2025-10-01 10:54:12 +0200

    feat: adds modal to ci/cd -- test

---
+++ cognee/tests/test_distributed.py
@@ -0,0 +1,14 @@
+import modal
+
+app = modal.App("modal-ci-cd-github-test")
+
+
+@app.function()
+def square(x):
+    print("This code is running on a remote worker!")
+    return x**2
+
+
+@app.local_entrypoint()
+def main():
+    print("the square is", square.remote(42))
