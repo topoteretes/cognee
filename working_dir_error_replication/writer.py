d537d6225c385ceae8ce789b926b0a7fc877e0fa
hajdul88 <52442977+hajdul88@users.noreply.github.com>
2025-10-06 13:19:18 +0200

    chore: adds error reproduction files (to delete later)

---
+++ working_dir_error_replication/writer.py
@@ -0,0 +1,31 @@
+import asyncio
+from uuid import uuid4
+from cognee.infrastructure.databases.graph.kuzu.adapter import KuzuAdapter
+import time
+import uuid
+from cognee.modules.chunking.models.DocumentChunk import DocumentChunk
+from cognee.modules.data.processing.document_types import PdfDocument
+
+
+def create_node(name):
+    document = PdfDocument(
+        id=uuid.uuid4(),
+        name=name,
+        raw_data_location=name,
+        external_metadata="",
+        mime_type="",
+    )
+    return document
+
+async def main():
+    adapter = KuzuAdapter("test.db")
+    print("Writer: Starting...")
+    nodes = [create_node(f"Node{i}") for i in range(100)]
+    await adapter.add_nodes(nodes)
+
+    print("writer finished...")
+
+    time.sleep(100)
+
+if __name__ == "__main__":
+    asyncio.run(main())
