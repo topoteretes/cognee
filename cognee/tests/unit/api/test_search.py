4e2a7778600bcea3992dbec4466939022e9b53c8
Daulet Amirkhanov <damirkhanov01@gmail.com>
2025-10-17 14:18:44 +0100

    tests: update tests after last refactoring

--- cognee/tests/unit/api/test_search.py
+++ cognee/tests/unit/api/test_search.py
@@ -1,6 +1,5 @@
 import pytest
 import cognee
-from cognee.modules.data.exceptions import SearchOnEmptyGraphError
 
 
 @pytest.mark.asyncio
@@ -8,16 +7,15 @@ async def test_empty_search_raises_SearchOnEmptyGraphError_on_empty_graph():
     await cognee.prune.prune_data()
     await cognee.prune.prune_system(metadata=True)
     await cognee.add("Sample input")
-    with pytest.raises(SearchOnEmptyGraphError):
-        await cognee.search("Sample query")
+    result = await cognee.search("Sample query")
+    assert result == []
 
 
+@pytest.mark.asyncio
 async def test_empty_search_doesnt_raise_SearchOnEmptyGraphError():
     await cognee.prune.prune_data()
     await cognee.prune.prune_system(metadata=True)
     await cognee.add("Sample input")
     await cognee.cognify()
-    try:
-        await cognee.search("Sample query")
-    except SearchOnEmptyGraphError:
-        pytest.fail("Should not raise SearchOnEmptyGraphError when data was added and cognified")
+    result = await cognee.search("Sample query")
+    assert result != []
