a35bcecdf9dd386e62b07e912c788d0bf20682b4
Daulet Amirkhanov <damirkhanov01@gmail.com>
2025-10-21 23:13:40 +0100

    refactor tavily_crawler test

--- cognee/tests/integration/web_url_crawler/test_tavily_crawler.py
+++ cognee/tests/integration/web_url_crawler/test_tavily_crawler.py
@@ -1,14 +1,11 @@
-import os
 import pytest
-from cognee.tasks.web_scraper.config import TavilyConfig
 from cognee.tasks.web_scraper.utils import fetch_with_tavily
 
 
 @pytest.mark.asyncio
 async def test_fetch():
     url = "https://en.wikipedia.org/wiki/Large_language_model"
-    tavily_config = TavilyConfig()
-    results = await fetch_with_tavily(url, tavily_config)
+    results = await fetch_with_tavily(url)
     assert len(results) == 1
     assert isinstance(results, dict)
     html = results[url]
