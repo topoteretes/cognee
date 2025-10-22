c5648e63375d9eb1520f5a007dda520f70c9c145
Andrej Milicevic <milicevi@Andrejs-MacBook-Pro.local>
2025-10-22 09:22:11 +0200

    test: Add load test.

--- cognee/tests/test_load.py
+++ cognee/tests/test_load.py
@@ -9,8 +9,8 @@ from cognee.shared.logging_utils import get_logger
 
 logger = get_logger()
 
-async def process_and_search(num_of_searches):
 
+async def process_and_search(num_of_searches):
     start_time = time.time()
 
     await cognee.cognify()
@@ -26,26 +26,34 @@ async def process_and_search(num_of_searches):
 
     return end_time - start_time
 
-async def main():
 
+async def main():
     file_path = os.path.join(
         pathlib.Path(__file__).resolve().parent, "test_data/artificial-intelligence.pdf"
     )
+    data_directory_path = str(
+        pathlib.Path(
+            os.path.join(pathlib.Path(__file__).parent, ".data_storage/test_load")
+        ).resolve()
+    )
+    cognee.config.data_root_directory(data_directory_path)
+    cognee_directory_path = str(
+        pathlib.Path(
+            os.path.join(pathlib.Path(__file__).parent, ".cognee_system/test_load")
+        ).resolve()
+    )
+    cognee.config.system_root_directory(cognee_directory_path)
 
     num_of_pdfs = 10
     num_of_reps = 5
-    upper_boundary_minutes = 3
-    average_minutes = 1.5
+    upper_boundary_minutes = 10
+    average_minutes = 8
 
     await cognee.prune.prune_data()
     await cognee.prune.prune_system(metadata=True)
 
-    await asyncio.gather(
-        *[
-            cognee.add(file_path, dataset_name=f"dataset_{i}")
-            for i in range(num_of_pdfs)
-        ]
-    )
+    for i in range(num_of_pdfs):
+        await cognee.add(file_path, dataset_name=f"dataset_{i}")
 
     recorded_times = await asyncio.gather(
         *[process_and_search(num_of_pdfs) for _ in range(num_of_reps)]
