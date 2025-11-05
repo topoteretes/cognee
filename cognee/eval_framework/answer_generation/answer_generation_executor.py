12c077aa84fe98a398f8406c49947e608bf4fe5b
hajdul88 <52442977+hajdul88@users.noreply.github.com>
2025-11-05 11:40:18 +0100

    ruff fix

--- cognee/eval_framework/answer_generation/answer_generation_executor.py
+++ cognee/eval_framework/answer_generation/answer_generation_executor.py
@@ -38,7 +38,9 @@ class AnswerGeneratorExecutor:
             ############
             #:TODO This is a quick fix until we don't structure retriever results properly but lets not leave it like this...this is needed now due to the changed combined retriever structure..
             if not isinstance(retrieval_context, str) and isinstance(retrieval_context, list):
-                retrieval_context=await retriever.convert_retrieved_objects_to_context(triplets = retrieval_context)
+                retrieval_context = await retriever.convert_retrieved_objects_to_context(
+                    triplets=retrieval_context
+                )
 
             if isinstance(search_results, str):
                 search_results = [search_results]
