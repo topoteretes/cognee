3bc3f63362978b51f896235819c0e709e603f29c
Igor Ilic <igorilic03@gmail.com>
2025-12-19 11:55:13 +0100

    fix: Resolve issues with migration

--- alembic/versions/e1ec1dcb50b6_add_last_accessed_to_data.py
+++ alembic/versions/e1ec1dcb50b6_add_last_accessed_to_data.py
@@ -15,7 +15,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = "e1ec1dcb50b6"
-down_revision: Union[str, None] = "211ab850ef3d"
+down_revision: Union[str, None] = "46a6ce2bd2b2"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
