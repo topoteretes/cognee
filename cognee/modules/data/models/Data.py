e29a7df45ac3aa856ee36050a4f778841e54d06c
Anushree2005 <anushree.revankar102@gamil.com>
2025-12-27 17:57:03 +0530

    feat(data): add optional label field to Data model

--- cognee/modules/data/models/Data.py
+++ cognee/modules/data/models/Data.py
@@ -29,6 +29,7 @@ class Data(Base):
     external_metadata = Column(JSON)
     # Store NodeSet as JSON list of strings
     node_set = Column(JSON, nullable=True)
+    label = Column(String, nullable=True)
     # MutableDict allows SQLAlchemy to notice key-value pair changes, without it changing a value for a key
     # wouldn't be noticed when commiting a database session
     pipeline_status = Column(MutableDict.as_mutable(JSON))
