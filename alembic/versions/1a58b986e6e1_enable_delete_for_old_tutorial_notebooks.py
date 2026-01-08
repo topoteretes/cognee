7de3356b1f8cd217a9bbbef8ac595c333197902c
Igor Ilic <igorilic03@gmail.com>
2026-01-08 14:28:39 +0100

    fix: Resolve issue with migration order

--- alembic/versions/1a58b986e6e1_enable_delete_for_old_tutorial_notebooks.py
+++ alembic/versions/1a58b986e6e1_enable_delete_for_old_tutorial_notebooks.py
@@ -14,7 +14,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = "1a58b986e6e1"
-down_revision: Union[str, None] = "46a6ce2bd2b2"
+down_revision: Union[str, None] = "e1ec1dcb50b6"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
