33362118b89e039bc4ef0859d7759aa1eb5f6f3a
hajdul88 <52442977+hajdul88@users.noreply.github.com>
2026-01-08 18:00:25 +0100

    fix: fixing permissions to see all ag_catalog related operations

--- postgres-age-vector/init/01_init_extensions.sql
+++ postgres-age-vector/init/01_init_extensions.sql
@@ -1,8 +1,19 @@
-CREATE EXTENSION IF NOT EXISTS vector;
+CREATE EXTENSION IF NOT EXISTS vector WITH SCHEMA public;
 
 LOAD 'age';
-
 CREATE EXTENSION IF NOT EXISTS age;
 
-GRANT USAGE ON SCHEMA ag_catalog TO cognee;
+GRANT USAGE ON SCHEMA ag_catalog TO PUBLIC;
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA ag_catalog TO cognee;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA ag_catalog TO cognee;
 GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA ag_catalog TO cognee;
+
+ALTER DATABASE cognee_db SET search_path = public, ag_catalog, "$user";
+
+DO $$
+BEGIN
+    RAISE NOTICE 'pgvector: % | AGE: % | search_path: public, ag_catalog',
+        (SELECT extversion FROM pg_extension WHERE extname = 'vector'),
+        (SELECT extversion FROM pg_extension WHERE extname = 'age');
+END $$;
