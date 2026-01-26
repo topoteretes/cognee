6308743cbec52237226b06ceb1b6c852ff8dbc8c
Milenko Gavric <milenko.gavric@gmail.com>
2026-01-26 14:37:17 +0100

    removed example link from comprehensive_example

--- new-examples/demos/comprehensive_example/cognee_comprehensive_example.py
+++ new-examples/demos/comprehensive_example/cognee_comprehensive_example.py
@@ -1,10 +1,3 @@
-"""
-Core Features Getting Started Example
-
-Reference: https://colab.research.google.com/drive/12Vi9zID-M3fpKpKiaqDBvkk98ElkRPWy?usp=sharing
-
-"""
-
 import os
 import cognee
 import asyncio
