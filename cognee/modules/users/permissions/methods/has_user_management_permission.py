36a9c5fd0f393f9fd5f84c5e80bdf1da7def1d06
vanika02 <vanikadangi02@gmail.com>
2026-01-28 06:42:53 +0000

    Add user management permission check for tenant owners

--- cognee/modules/users/permissions/methods/has_user_management_permission.py
+++ cognee/modules/users/permissions/methods/has_user_management_permission.py
@@ -4,9 +4,12 @@ from cognee.modules.users.permissions.methods.get_tenant import get_tenant
 from cognee.modules.users.exceptions import PermissionDeniedError
 
 
-async def has_user_management_permission(requester_id: UUID, tenant_id: UUID) -> bool:
+async def has_user_management_permission(
+    requester_id: UUID,
+    tenant_id: UUID
+) -> bool:
     """
-    Check if requester is allowed to manage users for a tenant.
+        Check if requester is allowed to manage users for a tenant.
     Args:
         requester_id: Id of the user making the request
         tenant_id: Id of the tenant
@@ -15,8 +18,7 @@ async def has_user_management_permission(requester_id: UUID, tenant_id: UUID) ->
         True if requester has permission
 
     Raises:
-        PermissionDeniedError: If requester is not authorized.
-        TenantNotFoundError: If the tenant does not exist.
+        PermissionDeniedError: If requester is not authorized
     """
     tenant = await get_tenant(tenant_id)
 
