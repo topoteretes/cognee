0e331a6e0ab1aa29c44e1a68411e1f501a0bd00d
Omkar Kabde <omkarkabde@gmail.com>
2026-01-29 00:58:35 +0530

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-from cognee.api.v1.config.config import config
-
-config.set_vector_db_config({
-    "vector_db_provider": "lancedb",  # valid
-    "vector_db_urll": "/tmp/db",  # typo: invalid key
-})
