9eaf62ba9a55ca4b1354712d78581423c76cf77d
Milenko Gavric <milenko.gavric@gmail.com>
2026-02-17 13:30:36 +0100

    restore graph_visualization to original

--- examples/guides/graph_visualization.py
+++ examples/guides/graph_visualization.py
@@ -2,17 +2,12 @@ import asyncio
 import cognee
 from cognee.api.v1.visualize.visualize import visualize_graph
 
-from os import path
-
 
 async def main():
-    await cognee.prune.prune_data()
-    await cognee.prune.prune_system(metadata=True)
     await cognee.add(["Alice knows Bob.", "NLP is a subfield of CS."])
     await cognee.cognify()
 
-    graph_visualization_path = path.join(path.dirname(__file__), "./graph_after_cognify.html")
-    await visualize_graph(graph_visualization_path)
+    await visualize_graph("./graph_after_cognify.html")
 
 
 if __name__ == "__main__":
