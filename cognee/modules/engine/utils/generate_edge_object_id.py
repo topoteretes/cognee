00fdb349e06d435625aab4eda32e3e02d030291e
hajdul88 <52442977+hajdul88@users.noreply.github.com>
2026-03-02 12:38:11 +0100

    ruff

--- cognee/modules/engine/utils/generate_edge_object_id.py
+++ cognee/modules/engine/utils/generate_edge_object_id.py
@@ -9,6 +9,9 @@ def generate_edge_object_id(
     Used for feedback mapping and stored on edges in the graph DB as edge_object_id.
     """
     edge_specific_identifier = (
-        str(source_node_id) + relationship_name + str(target_node_id)
-    ).lower().replace(" ", "_").replace("'", "")
+        (str(source_node_id) + relationship_name + str(target_node_id))
+        .lower()
+        .replace(" ", "_")
+        .replace("'", "")
+    )
     return str(uuid5(NAMESPACE_OID, edge_specific_identifier))
