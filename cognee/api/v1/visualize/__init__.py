ab117eb25b40a05a8afeb4f04196a5eb09a5f32b
mohammadehsanansari <mdehsan873@gmail.com>
2026-03-09 14:36:33 +0530

    hotfix start_visualization_server

--- cognee/api/v1/visualize/__init__.py
+++ cognee/api/v1/visualize/__init__.py
@@ -1,2 +1,10 @@
 from .visualize import visualize_graph, visualize_multi_user_graph
 from .start_visualization_server import visualization_server
+from cognee.shared.utils import start_visualization_server
+
+__all__ = [
+    "visualize_graph",
+    "visualize_multi_user_graph",
+    "visualization_server",
+    "start_visualization_server",
+]
