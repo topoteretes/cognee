02aaa35e821446f3a6802548c3459075d52ae36d
Igor Ilic <igorilic03@gmail.com>
2026-03-17 22:26:04 +0100

    fix: add support for sqlite + pgvector combo

--- cognee/infrastructure/databases/relational/sqlalchemy/SqlAlchemyAdapter.py
+++ cognee/infrastructure/databases/relational/sqlalchemy/SqlAlchemyAdapter.py
@@ -565,7 +565,10 @@ class SQLAlchemyAdapter:
             from cognee.infrastructure.databases.vector.config import get_vectordb_config
 
             vector_config = get_vectordb_config()
-            if vector_config.vector_db_provider == "pgvector":
+            if (
+                vector_config.vector_db_provider == "pgvector"
+                and self.engine.dialect.name == "postgresql"
+            ):
                 await connection.execute(text("CREATE EXTENSION IF NOT EXISTS vector;"))
             if len(Base.metadata.tables.keys()) > 0:
                 await connection.run_sync(Base.metadata.create_all)
