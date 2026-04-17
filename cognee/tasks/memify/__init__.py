9b52c000b72808277af83b9995203861a67f44bc
hajdul88 <52442977+hajdul88@users.noreply.github.com>
2026-04-17 12:28:17 +0200

    chore: exposes additional tasks in init

--- cognee/tasks/memify/__init__.py
+++ cognee/tasks/memify/__init__.py
@@ -8,7 +8,9 @@ to support memory enrichment and downstream knowledge graph processing.
 
 from .extract_subgraph import extract_subgraph
 from .extract_subgraph_chunks import extract_subgraph_chunks
+from .cognify_agent_trace_feedback import cognify_agent_trace_feedback
 from .cognify_session import cognify_session
+from .extract_agent_trace_feedbacks import extract_agent_trace_feedbacks
 from .extract_user_sessions import extract_user_sessions
 from .extract_feedback_qas import extract_feedback_qas
 from .apply_feedback_weights import apply_feedback_weights
